1632562f74dd23a2977e11bc82a1f61b6ca27fb8
Bence Piros <gyorgybence.piros@lastpass.com>
2023-07-23 19:17:38 +0200

    Coroutine usecase 3 - initial faulty solution by me

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase3/PerformNetworkRequestsConcurrentlyViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase3/PerformNetworkRequestsConcurrentlyViewModel.kt
@@ -1,14 +1,37 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase3
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import com.lukaslechner.coroutineusecasesonandroid.mock.VersionFeatures
+import com.lukaslechner.coroutineusecasesonandroid.utils.runSuspendCatching
+import kotlinx.coroutines.launch
+import okhttp3.internal.Version
+import timber.log.Timber
 
 class PerformNetworkRequestsConcurrentlyViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequestsSequentially() {
+        viewModelScope.launch {
+            runSuspendCatching {
+                val recentVersions = mockApi
+                    .getRecentAndroidVersions()
+                    .sortedBy { it.apiLevel }
 
+                val list = buildList {
+                    recentVersions.forEach {version ->
+                        add(mockApi.getAndroidVersionFeatures(version.apiLevel))
+                    }
+                }
+
+                uiState.value = UiState.Success(list)
+            }.onFailure {
+                Timber.e(it.message)
+                uiState.value = UiState.Error("Something went wrong")
+            }
+        }
     }
 
     fun performNetworkRequestsConcurrently() {
