57beaea2156b074039a4362278b91bffb55ed190
Bence <pirosgyb@gmail.com>
2023-10-26 12:47:58 +0200

    Coroutine usecase 8 solution

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase8/RoomAndCoroutinesViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase8/RoomAndCoroutinesViewModel.kt
@@ -1,7 +1,10 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase8
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class RoomAndCoroutinesViewModel(
     private val api: MockApi,
@@ -9,11 +12,34 @@ class RoomAndCoroutinesViewModel(
 ) : BaseViewModel<UiState>() {
 
     fun loadData() {
+        uiState.value = UiState.Loading.LoadFromDb
 
+        viewModelScope.launch {
+            val localVersions = database.getAndroidVersions()
+            if (localVersions.isEmpty()) {
+                uiState.value = UiState.Error(DataSource.DATABASE, "Database empty!")
+            } else {
+                uiState.value =
+                    UiState.Success(DataSource.DATABASE, localVersions.mapToUiModelList())
+            }
+
+            uiState.value = UiState.Loading.LoadFromNetwork
+            try {
+                val recentVersions = api.getRecentAndroidVersions()
+                recentVersions.forEach { version ->
+                    database.insert(version.mapToEntity())
+                }
+                uiState.value = UiState.Success(DataSource.NETWORK, recentVersions)
+            } catch (e: Exception) {
+                uiState.value = UiState.Error(DataSource.NETWORK, "Network error!")
+            }
+        }
     }
 
     fun clearDatabase() {
-
+        viewModelScope.launch {
+            database.clear()
+        }
     }
 }
 
